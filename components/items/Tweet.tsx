602d6ddaf035479c963998b77f0aa73372b8d7c7
Gerardo <gerardopapaleco@gmail.com>
2021-12-17 01:43:48 -0400

    if parent show replying to

--- components/items/Tweet.tsx
+++ components/items/Tweet.tsx
@@ -16,58 +16,72 @@ interface Props {
 const Tweet: FC<Props> = memo(({ post, navigation }) => {
   return (
     <TouchableOpacity
-      style={tw`flex-row border-b border-gray-300 ml-1 px-2 py-2`}
+      style={tw`pl-1 border-b border-gray-300`}
       activeOpacity={0.9}
       onPress={() => navigation.push("Thread", post.id)}
     >
-      <UserPictureCircle author={post.author} navigation={navigation} />
-
-      {/* Right Side */}
-      <View style={tw`flex-1 ml-2`}>
-        {/* Username and date */}
-        <View style={tw`flex-row items-center mt-1 mb-2`}>
+      {post.parent ? (
+        <TouchableOpacity
+          style={tw`ml-3 mt-2`}
+          onPress={() => navigation.push("Thread", post.parent?.id)}
+        >
           <AppText>
-            <Text style={tw`font-bold`}>{post.author?.username}</Text>
-          </AppText>
-          <AppText>
-            <Text style={tw`font-bold opacity-60`}>
-              {" "}
-              • {parseDate(post.date!)}
+            <Text style={tw`text-gray-400 italic`}>
+              Replying to {post.parent.author?.username}
             </Text>
           </AppText>
-        </View>
+        </TouchableOpacity>
+      ) : null}
+      <View style={tw`flex-row  px-2 py-2`}>
+        <UserPictureCircle author={post.author} navigation={navigation} />
 
-        {/* Tweet text */}
-        {post.text ? (
-          <View style={tw`mb-2 -mt-1`}>
+        {/* Right Side */}
+        <View style={tw`flex-1 ml-2`}>
+          {/* Username and date */}
+          <View style={tw`flex-row items-center mt-1 mb-2`}>
             <AppText>
-              <Text style={tw`text-base w-full leading-tight`}>
-                {post.text}
+              <Text style={tw`font-bold`}>{post.author?.username}</Text>
+            </AppText>
+            <AppText>
+              <Text style={tw`font-bold opacity-60`}>
+                {" "}
+                • {parseDate(post.date!)}
               </Text>
             </AppText>
           </View>
-        ) : null}
 
-        {/* Image */}
-        {post.images?.length ? (
-          <View style={tw`overflow-visible mb-2`}>
-            <Image
-              key={post.images[0]}
-              source={{
-                uri: post.images[0].replace("/upload", "/upload/q_40"),
-              }}
-              style={tw`w-full h-[200px] rounded-xl`}
-            />
-          </View>
-        ) : null}
+          {/* Tweet text */}
+          {post.text ? (
+            <View style={tw`mb-2 -mt-1`}>
+              <AppText>
+                <Text style={tw`text-base w-full leading-tight`}>
+                  {post.text}
+                </Text>
+              </AppText>
+            </View>
+          ) : null}
 
-        {/* Touchable icons */}
-        <TweetButtons
-          onReply={() => {
-            navigation.push("Thread", post.id);
-          }}
-          post={post}
-        />
+          {/* Image */}
+          {post.images?.length ? (
+            <View style={tw`overflow-visible mb-2`}>
+              <Image
+                key={post.images[0]}
+                source={{
+                  uri: post.images[0].replace("/upload", "/upload/q_40"),
+                }}
+                style={tw`w-full h-[200px] rounded-xl`}
+              />
+            </View>
+          ) : null}
+
+          {/* Touchable icons */}
+          <TweetButtons
+            onReply={() => {
+              navigation.push("Thread", post.id);
+            }}
+            post={post}
+          />
+        </View>
       </View>
     </TouchableOpacity>
   );
