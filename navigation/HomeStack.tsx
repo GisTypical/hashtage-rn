35a5d4ff50edf0b17da2b0ae2ee36ce4dfb6efad
Gerardo <gerardopapaleco@gmail.com>
2021-12-17 04:07:23 -0400

    feed header icon

--- navigation/HomeStack.tsx
+++ navigation/HomeStack.tsx
@@ -1,12 +1,11 @@
 import { NavigationProp } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React, { useContext } from "react";
-import { TouchableOpacity } from "react-native";
-import AppText from "../components/AppText";
+import { Image } from "react-native";
 import YellowButton from "../components/buttons/YellowButton";
 import { AuthContext } from "../components/providers/AuthProvider";
 import Feed from "../screens/Feed";
-
+import tw from "../utils/tailwind";
 const Stack = createNativeStackNavigator();
 
 interface Props {
@@ -22,12 +21,19 @@ const HomeStack = ({ navigation }: Props) => {
         name="Feed"
         component={Feed}
         options={{
-          headerTitle: () => (
+          headerLeft: () => (
             <YellowButton
               text="Profile"
               onPress={() => navigation.navigate("Profile", user)}
             ></YellowButton>
           ),
+          headerTitle: () => (
+            <Image
+              style={tw`h-13 w-13`}
+              source={require("../assets/adaptive-icon.png")}
+            />
+          ),
+          headerTitleAlign: "center",
         }}
       ></Stack.Screen>
     </Stack.Navigator>
